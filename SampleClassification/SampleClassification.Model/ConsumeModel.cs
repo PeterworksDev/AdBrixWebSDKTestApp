9541590ff5a9a22f593275b6f174eb04653d95cf
jinukkr <jinukkr@gmail.com>
2021-05-01 14:34:57 +0900

    Add MLModel Project
    Signed-off-by: jin-ukpark <jinukkr@hotmail.com>

---
+++ SampleClassification/SampleClassification.Model/ConsumeModel.cs
@@ -0,0 +1,40 @@
+//*****************************************************************************************
+//*                                                                                       *
+//* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
+//*                                                                                       *
+//*****************************************************************************************
+
+using System;
+using System.IO;
+using Microsoft.ML;
+
+namespace SampleClassification.Model
+{
+    public class ConsumeModel
+    {
+        private static readonly Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictionEngine =
+            new Lazy<PredictionEngine<ModelInput, ModelOutput>>(CreatePredictionEngine);
+
+        public static string MLNetModelPath = Path.GetFullPath("MLModel.zip");
+
+        // For more info on consuming ML.NET models, visit https://aka.ms/mlnet-consume
+        // Method for consuming model in your app
+        public static ModelOutput Predict(ModelInput input)
+        {
+            var result = PredictionEngine.Value.Predict(input);
+            return result;
+        }
+
+        public static PredictionEngine<ModelInput, ModelOutput> CreatePredictionEngine()
+        {
+            // Create new MLContext
+            var mlContext = new MLContext();
+
+            // Load model & create prediction engine
+            var mlModel = mlContext.Model.Load(MLNetModelPath, out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            return predEngine;
+        }
+    }
+}
